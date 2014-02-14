48ad3964f82b7aaa06ef47f7c78b2108826bf728
agustinventura <agustinventura@gmail.com>
2014-02-14 08:13:47 +0100

    latest changes

--- src/main/java/es/aguasnegras/tdd/MathOperator.java
+++ src/main/java/es/aguasnegras/tdd/MathOperator.java
@@ -3,20 +3,20 @@ package es.aguasnegras.tdd;
 /**
  * Created by aventura on 10/02/14.
  */
-public class MathOperator {
+public abstract class MathOperator {
 
     private final int precedence;
     private final int index;
-    private final MathToken token;
+    private final String token;
 
-    public MathOperator(int precedence, int index, MathToken token) {
+    public MathOperator(int precedence, int index, String token) {
         this.precedence = precedence;
         this.index = index;
         this.token = token;
     }
 
-    public String getValue() {
-        return token.getValue();
+    public String getToken() {
+        return token;
     }
 
     public int getPrecedence() {
@@ -27,28 +27,5 @@ public class MathOperator {
         return index;
     }
 
-    public int resolve(int firstOperand, int secondOperand, CalculatorProxy calculator) {
-        int result = 0;
-        switch (token.getValue()) {
-            case "*":
-                result = calculator.binaryOperation(CalculatorProxy.CalculatorMethod.MULTIPLY, firstOperand,
-                        secondOperand);
-                break;
-            case "/":
-                result = calculator.binaryOperation(CalculatorProxy.CalculatorMethod.DIVIDE, firstOperand,
-                        secondOperand);
-                break;
-            case "+":
-                result = calculator.binaryOperation(CalculatorProxy.CalculatorMethod.ADD, firstOperand,
-                        secondOperand);
-                break;
-            case "-":
-                result = calculator.binaryOperation(CalculatorProxy.CalculatorMethod.SUBSTRACT, firstOperand,
-                        secondOperand);
-                break;
-            default:
-                break;
-        }
-        return result;
-    }
+    public abstract int resolve(int firstOperand, int secondOperand, CalculatorProxy calculator);
 }
---
+++ src/main/java/es/aguasnegras/tdd/MultiplyOperator.java
@@ -0,0 +1,11 @@
+package es.aguasnegras.tdd;
+
+/**
+ * Created by aventura on 13/02/14.
+ */
+public class MultiplyOperator extends MathOperator {
+
+    public MultiplyOperator() {
+        super()
+    }
+}
